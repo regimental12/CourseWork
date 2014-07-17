4d351eb82c5dbdd9d531e82e6d92f8289aaf7911
regimental12 <richard.hankins@gmail.com>
2014-07-17 13:29:17 +0100

    capped FrameRate

--- CourseWork/main.cpp
+++ CourseWork/main.cpp
@@ -1,18 +1,29 @@
 #include "Game.h"
 
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
+
 int main(int argc, char* args[])
 {
+	Uint32 frameStart , frameTime;
+
 	std::cout << "init game attempt...\n";
 	if (TheGame::Instance()->init("chapter 1" , 100 , 100 , 640 , 480 , false))
 	{
 		std::cout << "game init success\n";
 		while (TheGame::Instance()->running())
 		{
+			frameStart = SDL_GetTicks();
+
 			TheGame::Instance()->handleEvents();
 			TheGame::Instance()->update();
 			TheGame::Instance()->render();
 
-			SDL_Delay(10);
+			frameTime = SDL_GetTicks() - frameStart;
+			if (frameTime < DELAY_TIME)
+			{
+				SDL_Delay((int)(DELAY_TIME - frameTime));
+			}
 		}
 	}
 	else
